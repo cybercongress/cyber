9c08e2201fe4c847387cb3339621b2743da96d71
Cheslav Zhuravsky <happylolonly@gmail.com>
2024-06-24 11:39:56 +0700

    update build

--- build/static/js/custom.js
+++ build/static/js/custom.js
@@ -1,2 +1,6 @@
 document.documentElement.setAttribute("data-theme", "dark");
 document.documentElement.setAttribute("data-color", "green");
+
+document.documentElement.classList.add("dark");
+
+document.querySelector(".journals-nav .flex-1").innerHTML = "Blog";
