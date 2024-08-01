58e65df1b769ecc40a6f5d848029967900171e26
mrlp4 <laptanovi4@gmail.com>
2024-08-01 17:43:30 +0200

    script insert fixes

--- .github/scripts/modify_html/add_meta.sh
+++ .github/scripts/modify_html/add_meta.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-metas='    <meta property="og:title" content="cyber docs">
-    <meta name="description" content="cyber docs">
-    <meta property="og:description" content="cyber docs">
-    <meta property="og:image" content="/static/img/logo.png">'
+metas='<meta property="og:title" content="cyber docs">
+
+<meta name="description" content="cyber docs">
+<meta property="og:description" content="cyber docs">
+
+<meta property="og:image" content="/static/img/logo.png">'
 
 file="$GITHUB_WORKSPACE/build/index.html"
 
