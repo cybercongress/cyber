30884430254811b9b9e04fa9fe9d1ad0f1fdc2b3
mrlp4 <laptanovi4@gmail.com>
2024-08-01 16:51:03 +0200

    debug

--- .github/scripts/modify_html/add_meta.sh
+++ .github/scripts/modify_html/add_meta.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-metas=$(cat <<'EOF'
-    <meta property="og:title" content="cyber docs">
-
+metas='    <meta property="og:title" content="cyber docs">
     <meta name="description" content="cyber docs">
     <meta property="og:description" content="cyber docs">
+    <meta property="og:image" content="/static/img/logo.png">'
 
-    <meta property="og:image" content="/static/img/logo.png">
-EOF
-)
+file="$GITHUB_WORKSPACE/build/index.html"
 
-sed "/<\/head>/i\\ $metas" "$GITHUB_WORKSPACE/build/index.html"
+sed -i "/<\/head>/i\\
+$(echo "$metas" | sed 's/$/\\n/' | tr -d '\n')\\
+" "$file"
