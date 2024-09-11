7a5bc3b08a737f15c8c7242a5d6d7197996f4bb5
Cheslav Zhuravsky <happylolonly@gmail.com>
2024-09-11 12:50:22 +0700

    force change theme to black

--- logseq/custom.js
+++ logseq/custom.js
@@ -1,1 +1,10 @@
+// change theme to black
+if (document.querySelector("html").getAttribute("data-theme") === "light") {
+  document.querySelector("html").setAttribute("data-theme", "dark");
+
+  document.querySelector("body").classList.remove("light-theme");
+  document.querySelector("body").classList.remove("white-theme");
+  document.querySelector("body").classList.add("dark-theme");
+}
+
 document.querySelector(".journals-nav .flex-1").innerHTML = "Blog";
